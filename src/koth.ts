2e7152797177183972fa592fb0554eac2feff871
kontrol <kontrol_apa@protonmail.com>
2022-06-16 21:59:23 +0200

    Initial commit

---
+++ src/koth.ts
@@ -0,0 +1,66 @@
+import { BigInt } from "@graphprotocol/graph-ts"
+import {
+  KOTH,
+  NewKing,
+  UpdatedOwner,
+  WinnerChanged
+} from "../generated/KOTH/KOTH"
+import { ExampleEntity } from "../generated/schema"
+
+export function handleNewKing(event: NewKing): void {
+  // Entities can be loaded from the store using a string ID; this ID
+  // needs to be unique across all entities of the same type
+  let entity = ExampleEntity.load(event.transaction.from.toHex())
+
+  // Entities only exist after they have been saved to the store;
+  // `null` checks allow to create entities on demand
+  if (!entity) {
+    entity = new ExampleEntity(event.transaction.from.toHex())
+
+    // Entity fields can be set using simple assignments
+    entity.count = BigInt.fromI32(0)
+  }
+
+  // BigInt and BigDecimal math are supported
+  entity.count = entity.count + BigInt.fromI32(1)
+
+  // Entity fields can be set based on event parameters
+  entity.cycle = event.params.cycle
+  entity.winner_adr = event.params.winner.adr
+
+  // Entities can be written to the store with `.save()`
+  entity.save()
+
+  // Note: If a handler doesn't require existing field values, it is faster
+  // _not_ to load the entity from the store. Instead, create it fresh with
+  // `new Entity(...)`, set the fields that should be updated and save the
+  // entity back to the store. Fields that were not set or unset remain
+  // unchanged, allowing for partial updates to be applied.
+
+  // It is also possible to access smart contracts from mappings. For
+  // example, the contract that has emitted the event can be connected to
+  // with:
+  //
+  // let contract = Contract.bind(event.address)
+  //
+  // The following functions can then be called on this contract to access
+  // state variables and other data:
+  //
+  // - contract.FLY(...)
+  // - contract.currentWinner(...)
+  // - contract.cycle(...)
+  // - contract.cycleToKing(...)
+  // - contract.enterAmount(...)
+  // - contract.getEnterAmount(...)
+  // - contract.isTimeUp(...)
+  // - contract.oldKings(...)
+  // - contract.owner(...)
+  // - contract.period(...)
+  // - contract.reward(...)
+  // - contract.startTs(...)
+  // - contract.step(...)
+}
+
+export function handleUpdatedOwner(event: UpdatedOwner): void {}
+
+export function handleWinnerChanged(event: WinnerChanged): void {}
